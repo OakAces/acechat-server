97a50cf9ce33ab3ab496e054d96c7a9d8f2a71e6
Daniel Dyla <dyladan@gmail.com>
2016-10-13 10:40:59 -0400

    Switched from TCP Sockets to WebSockets

--- acechat/user.py
+++ acechat/user.py
@@ -1,12 +1,15 @@
 class User:
-    def __init__(self, conn, addr):
+    def __init__(self, conn, path):
         """User class constructor"""
-        self.addr = ""
+        self.path = ""
         self.username = ""
         self.channels = list()
 
-        self.addr = addr
+        self.path = path
         self.conn = conn
+
+    def set_username(self, username):
+        self.username = username
 
     def has_username(self):
         return bool(self.username)
--- run.py
+++ run.py
@@ -1,9 +1,20 @@
 import acechat.server
+import websockets
+import asyncio
+import logging
+
+logger = logging.getLogger('websockets.server')
+logger.setLevel(logging.ERROR)
+logger.addHandler(logging.StreamHandler())
 
 addr = "localhost"
 port = 9090
 
 if __name__ == "__main__":
-    serv = acechat.Server(addr, port)
+    serv = acechat.Server()
 
-    serv.serve_forever()
+    start_server = websockets.serve(serv.handler, addr, port)
+
+    asyncio.get_event_loop().run_until_complete(start_server)
+    asyncio.get_event_loop().run_forever()
+
